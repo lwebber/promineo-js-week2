527943a5b26ea6199f1ac344f08a75066019ca9c
LAURA WEBBER <hello@lwebber.dev>
2021-01-30 11:42:53 -0800

    add variables

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-let age = 44;
-console.log(age);
+var isHotOutside = false;
+var isWeekday = false;
+var hasMoneyInPocket = false;
+
+var costOfMilke = 3.00;
+var moneyInWallet = 10.00;
+var thirstLevel = 5;
+

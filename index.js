4b0637e67c1018229a87b2f36186fb3893a82228
LAURA WEBBER <hello@lwebber.dev>
2021-01-30 11:50:04 -0800

    add expressions

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ var isHotOutside = false;
 var isWeekday = false;
 var hasMoneyInPocket = false;
 
-var costOfMilke = 3.00;
+var costOfMilk = 3.00;
 var moneyInWallet = 10.00;
 var thirstLevel = 5;
 
+var shouldBuyIceCream = isHotOutside && hasMoneyInPocket;
+var willGoSwimming = isHotOutside && !isWeekday;
+var isAGoodDay = isHotOutside && hasMoneyInPocket && !isWeekday;
+
+var willBuyMilk = (isHotOutside && thirstLevel >= 3) && moneyInWallet >= (2 * costOfMilk);
+
+/* willBuyMilk – this should be true if it is hot outside, 
+and thirstLevel is greater than or equal to 3, and 
+moneyInWallet is greater than or equal to 2 times the cost of milk
+*/
+console.log(isHotOutside);
+console.log(isWeekday);
+console.log(hasMoneyInPocket);
+
+console.log(costOfMilk);
+console.log(moneyInWallet);
+console.log(thirstLevel);
+
+console.log(shouldBuyIceCream);
+console.log(willGoSwimming);
+console.log(isAGoodDay);
+
+console.log(willBuyMilk);

b54d6f5d8e54e26161998aba401b94996ad4003d
LAURA WEBBER <hello@lwebber.dev>
2021-02-03 12:19:57 -0800

    move files

---
+++ homework/index.js
@@ -0,0 +1,36 @@
+/* var isHotOutside = false;
+var isWeekday = false;
+var hasMoneyInPocket = false;
+
+var costOfMilk = 3.00;
+var moneyInWallet = 10.00;
+var thirstLevel = 5;
+
+var shouldBuyIceCream = isHotOutside && hasMoneyInPocket;
+var willGoSwimming = isHotOutside && !isWeekday;
+var isAGoodDay = isHotOutside && hasMoneyInPocket && !isWeekday;
+
+var willBuyMilk = (isHotOutside && thirstLevel >= 3) && moneyInWallet >= (2 * costOfMilk);
+
+/* willBuyMilk – this should be true if it is hot outside, 
+and thirstLevel is greater than or equal to 3, and 
+moneyInWallet is greater than or equal to 2 times the cost of milk
+*/
+/* console.log(isHotOutside);
+console.log(isWeekday);
+console.log(hasMoneyInPocket);
+
+console.log(costOfMilk);
+console.log(moneyInWallet);
+console.log(thirstLevel);
+
+console.log(shouldBuyIceCream);
+console.log(willGoSwimming);
+console.log(isAGoodDay);
+
+console.log(willBuyMilk); */ 
+
+/*Given two nums, return their sum. Unless the two values are the same, 
+then return double their sum.*/
+
+var num1 = prompt('first number?');
